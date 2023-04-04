d087831440714e08bb3a432415f0be8cf1e2a23f
brendan-chess <brendanchess25@gmail.com>
2023-04-03 19:02:29 -0700

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import styles from "./styles/App.module.css";
+
+function App() {
+  return (
+    <div className={styles.container}>
+      <div className={styles.intro}>Hi, I'm Brendan</div>
+      <div>I'm a Front-end Web Developer</div>
+      <div>My Tech Stack:</div>
+      <div>React, Typescript</div>
+    </div>
+  );
+}
+
+export default App;
